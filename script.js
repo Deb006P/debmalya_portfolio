85bbf6cc95fe18fc0cc32a719d1047c97c0f9a82
Deb006P <debmalyapan4@gmail.com>
2024-08-04 22:33:04 +0530

    Add file till now responsive is not working

---
+++ script.js
@@ -0,0 +1,39 @@
+function redirect() {
+    window.location = ""
+}
+function redirect_2() {
+    window.location = "redirect/CV.pdf"
+}
+
+function redirect_4() {
+    window.location = "linkedin.com/in/debmalyapan"
+}
+// function scroll() {
+//     window.scrollTo(0, document.getElementsById("sec_3").offsetTop);
+// }
+
+
+// let sections = document.querySelectorAll('section')
+// let navLinks = document.querySelectorAll('header  nav a')
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset =sec.offsetTop -150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id')
+//          if(top >= offset && top < offset + height){
+//             navLinks.forEach(links =>{
+//                 links.classList.remove('active');
+//                 document.querySelector('header  nav a [herf*='+ id +']').classList.add('active');
+//             })
+//          }
+//     });
+
+// }
+
+
+// document.getElementbyId("about").addEventListener("click",scrollWin)
+// function alert_1() {
+//     window.alert ("Open GitHub")
+    
+// }
